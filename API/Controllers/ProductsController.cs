ac66d8159408daddf8f6c5edc270a72c1952eb8c
ugurkarakas <ugurkarakas@stu.ayvansaray.edu.tr>
2021-12-08 22:52:10 +0300

    test

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -31,6 +31,7 @@ namespace API.Controllers
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
             return await _context.Products.FindAsync(id);
+            //test commits
         }
     }
 }
