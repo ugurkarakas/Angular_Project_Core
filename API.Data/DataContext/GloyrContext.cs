a64ce1ed98b90739e49c9c04f90a68b43d14a840
ugurkarakas <ugurkarakas@stu.ayvansaray.edu.tr>
2021-12-08 22:48:42 +0300

    Add project files.

---
+++ API.Data/DataContext/GloyrContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Data.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data.DataContext
+{
+
+    public class GloyrContext : DbContext
+    {
+        public GloyrContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
